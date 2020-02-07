b4942bd747cbb58e91e22dbc27af7e774acfa982
Ja-boop <agusmisiones@gmail.com>
2020-02-06 21:35:12 -0300

    Pude poner los nombres, pero me falta agregarles el URL

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@
 fetch("https://pokeapi.co/api/v2/pokemon/")
 .then(respuesta => respuesta.json())
 .then(respuestaJSON => {
-    console.log(respuestaJSON)
+    console.log(respuestaJSON.results);
+    let listaNombreDePokemons;
 
-    Object.keys(respuestaJSON.results).forEach(nombrePokemon => {
-        $("h5").append($(`<p name="nombre-pokemon">${nombrePokemon}</p>`));
-    });
+    sacarLosNombresDeLosPokemon();
 
+    
+        
+    
+    
+
+    
+    
+
+    
+        function sacarLosNombresDeLosPokemon (){
+        for(i = 0; i <= 20; i++){
+            listaNombreDePokemons = respuestaJSON.results[0 + i].name;
+            $("ul").append($(`<p name="nombre-pokemon">${listaNombreDePokemons}</p>`));
+        }
+        
+    }
 })
+
