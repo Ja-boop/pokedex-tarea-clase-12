f7f92c6da0db59d1a33fc691db17e5cadfd427f5
Ja-boop <agusmisiones@gmail.com>
2020-02-28 19:14:42 -0300

    Ya puedo mostrar de que tipo es el pokemon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 /// <reference types="jquery" />
 
 let nextURL = null;
-let listaPokemon = document.querySelector('#lista-pokemon')
+let listaPokemon = document.querySelector('#lista-pokemon');
+let pokemones = document.querySelectorAll('.nombre-pokemon');
+let listaDeEstadisticasYFotos = document.querySelector('#estadisticas-fotos');
 
 
 listarPokemons();
@@ -11,37 +13,65 @@ function listarPokemons(url) {
     url = url || 'https://pokeapi.co/api/v2/pokemon'
 
     fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(pokemon => {
-            listadorDePokemon(pokemon.results);
-            nextURL = pokemon.next
+    .then(respuesta => respuesta.json())
+    .then(pokemon => {
+    
+        listadorDePokemon(pokemon.results);
+        nextURL = pokemon.next
+
+        $(".nombre-pokemon").click( (e) =>  {
+        
+            let linkDelPokemon = e.currentTarget.value;  
+        
+            fetch(linkDelPokemon)
+            .then(respuesta => respuesta.json())
+            .then(pokemon => {
+
+                console.log(pokemon.types[0].type.name);
+
+                mostrarFotosYEstadisticas(pokemon);
+
+            })
+
+        e.preventDefault()
         })
 
-        .catch(error => {
-            console.log(error + " Error en la peticion");
-        })
+    
+
+    })
+
+    .catch(error => {
+        console.log(error + " Error en la peticion");
+    })
 }
 
-let pintarPokemon = ``
+let ponerPokemon = ``
+let ponerFotosYEstadisticas = ``
+
+function mostrarFotosYEstadisticas(pokemon) {
+
+    $(listaDeEstadisticasYFotos).append(`<p>${pokemon.types[0].type.name}</p>`)
+
+}
 
 function listadorDePokemon(usuarios) {
 
     usuarios.map((pokemon) => {
 
-        pintarPokemon = `
-        <a href=${pokemon.url} 
-        target="_blank" rel="noopener noreferrer" name="nombre-pokemon">${pokemon.name}</a><br>
+        ponerPokemon = `
+        <button value=${pokemon.url} 
+        target="_blank" rel="noopener noreferrer" class="nombre-pokemon">${pokemon.name}</button><br>
         
         `
-
-        listaPokemon.innerHTML += pintarPokemon;
+      
+        listaPokemon.innerHTML += ponerPokemon;
     })
 }
 
 document.querySelector("#cambiar-pagina").addEventListener("click", (e) => {
-    console.log('No anda')
+    console.log('El boton funciona')
 
-    $("a").remove();
+    $(".nombre-pokemon").remove();
     $('br').remove();
 
     if (nextURL)
